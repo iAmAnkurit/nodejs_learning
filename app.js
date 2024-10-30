ab0e4215a543e904f9b4e2c2502c9539b9e4c63d
iAmAnkurit <bankurit1891@gmail.com>
2024-10-31 00:00:13 +0530

    Added cookie read and qrite

--- app.js
+++ app.js
@@ -59,11 +59,18 @@
 // app.listen(3000);
 
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.cookie("name", "Ankurit");
+  res.send("Done");
+});
+
+app.get("/read", (req, res) => {
+  console.log(req.cookies);
   res.send("Done");
 });
 
